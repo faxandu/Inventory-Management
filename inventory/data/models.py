70848c1f22e5d9579aa42bebd32ec7bc9a4e1853
faxandu <jmckeen@wccnet.edu>
2013-05-21 16:26:46 -0400

    start db & admin working

--- inventory/data/models.py
+++ inventory/data/models.py
@@ -4,15 +4,9 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Equipment(models.Model):
-	model_num = models.IntegerField()
-	serial_num = models.IntegerField()
-	equip_type = models.ForeignField(Equip_Type)
-	location = models.ForeignField(Location)
-	description = models.TextField()
-
-	def __unicode__(self):
-		return unicode(model_num)
+#number is implied
+class Equip_Type(models.Model):
+	eType = models.CharField(max_length=20, unique=True)
 
 class Location(models.Model):
 	room = models.CharField(max_length=4)
@@ -20,6 +14,12 @@ class Location(models.Model):
 	def __unicode__(self):
 		return unicode(self.building + self.room)
 
-#numer is implied
-class Equip_Type(models.Model):
-	eType = models.CharField(max_length=20, unique=True)
+class Equipment(models.Model):
+	model_num = models.IntegerField()
+	serial_num = models.IntegerField()
+	equip_type = models.ForeignKey(Equip_Type)
+	location = models.ForeignKey(Location)
+	description = models.TextField()
+
+	def __unicode__(self):
+		return unicode(model_num)
