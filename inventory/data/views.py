78f4fef5f318ffccb76460649cfe2cbe7734b121
faxandu <jmckeen@wccnet.edu>
2013-06-06 15:43:10 -0400

    Because serializers dident commit last time, for some reason

--- inventory/data/views.py
+++ inventory/data/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from data.serializers import UserSerializer
+from data.serializers import EquipSerializer
 from data.models import Equipment
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -9,4 +9,4 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Equipment.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = EquipSerializer
